022c937cb6c2a43d85dc30cb1a2762f0ca760e05
usha-sai <100438216+usha-sai@users.noreply.github.com>
2022-04-03 17:24:43 +0530

    Add files via upload

---
+++ connect.js
@@ -0,0 +1,16 @@
+var mysql = require('mysql');
+CREATE DATABASE todoapp;
+let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'todoapp'
+});
+
+connection.connect(function(err) {
+  if (err) {
+    return console.error('error: ' + err.message);
+  }
+
+  console.log('Connected to the MySQL server.');
+});
